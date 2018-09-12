1dcc28a50912635a4ba5a258270a837596e7d215
Kyle <kylelo@kyles-macbook-pro.local>
2018-09-11 22:20:18 -0400

    Initial Commit

---
+++ raytracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  raytracer
+//
+//  Created by kyle lo on 9/11/18.
+//  Copyright © 2018 kyle lo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
